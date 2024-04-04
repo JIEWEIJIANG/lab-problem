e3881ed863744ed829ed2fb00f00939d1cc6bfc8
JIEWEIJIANG <157995032+JIEWEIJIANG@users.noreply.github.com>
2024-04-05 01:08:10 +0800

    Update MyArrayList.java

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,1 +1,15 @@
+public class MyArrayList implements MyList{
+  int cap=4;
+  int size=0;
+  int [] arr = new int [cap];
 
+  @Override
+  public boolean add(int x){
+    arr[size++]=x;
+    return 0;
+  }
+
+  public int size(){
+    return size;
+  }
+}
